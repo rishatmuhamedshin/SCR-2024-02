5bb5273ae8be7566cf836d8b6446a12a2aa0bf54
Rishat Muhamedshin <risatmuhamedsin@gmail,com>
2024-04-28 12:03:09 +0300

    HomeWork 5

--- src/main/scala/module1/homework/futures/task_futures_sequence.scala
+++ src/main/scala/module1/homework/futures/task_futures_sequence.scala
@@ -16,10 +16,24 @@ object task_futures_sequence {
    * в правово результаты неуспешных выполнений.
    * Не допускается использование методов объекта Await и мутабельных переменных var
    */
+
   /**
    * @param futures список асинхронных задач
    * @return асинхронную задачу с кортежом из двух списков
    */
   def fullSequence[A](futures: List[Future[A]])
-                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = ???
+                     (implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+    val result = futures.foldRight(Future.successful(List[A](), List[Throwable]())) {
+      (future, lists) =>
+        lists.flatMap {
+          l =>
+            future.map { f => (f :: l._1, l._2) }
+              .recover {
+                case ex => (l._1, ex :: l._2)
+              }
+        }
+    }
+    result
+
+  }
 }
